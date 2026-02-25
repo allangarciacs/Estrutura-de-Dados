52e41e9b71e4347b599ae1e81f5b35076a1af3cb
Allan Storch Garcia <allan.storch21@gmail.com>
2026-02-25 11:09:21 -0300

    Add files via upload

---
+++ aulas/aula 02/exercicios/01.py
@@ -0,0 +1,21 @@
+lista_nomes = []
+
+#adicionar nomes na lista 
+#   lista_nomes.append("Guilherme")
+#   lista_nomes.append("Bruno")
+#   lista_nomes.append("Leonardo")
+
+#exibir a lista inteira
+print(lista_nomes)
+
+#remover um nome ada lista
+if len(lista_nomes) = 0:
+    nome = input("DIgite um nome: ")
+    if lista_nomes.__contains__(nome):
+        lista_nomes.remove(nome)
+    else: 
+        print(nome,"não esta na lista")
+
+    print(lista_nomes)
+else:
+    print("lista vazia...")
